1e55997e8e540d5f4bc4ceec206cbe2710e04435
Christopher Buja <csbuja@umich.edu>
2014-05-22 15:07:35 -0700

    now keeps track of a stat

--- testclient.js
+++ testclient.js
@@ -1,8 +1,11 @@
 var request = require('request');
+var StatsD = require('node-statsd').StatsD,
+      client = new StatsD();
 
 var postMe1 = {
         drink: 'coke',
-        drinker: 'fatguy'
+        drinker: 'Southern Gentleman',
+        status: 'admin'
     }
 console.log(postMe1.drink);
 var postMe2 = postMe1;
@@ -31,6 +34,28 @@ function callback(error, response, body) {
 
 for(i=0;i<10; ++ i)
 {
+  //make a metric to keep track of -- here we are doing 
+
+  //Parameters (specified as an options hash):
+
+  //host: The host to send stats to default: localhost
+  //port: The port to send stats to default: 8125
+  //more options to be posted later
+
+  //All StatsD methods have the same API:
+  //name: Stat name required
+  //value: Stat value required except in increment/decrement where it defaults to 1/-1 respectively
+  //sampleRate: Sends only a sample of data to StatsD default: 1
+  //callback: The callback to execute once the metric has been sent
+
+  //here is the current naming convention: 
+  //[Environment(may not be needed except for development].[product name/Cluster].[module-(streamsAPI/Keymaker/etc)].[component/topology/process].[bolt name].[metric (timer, increment/counter, gauge]
+
+  client.increment('prodstack.sodaCompany.KeymakerUI.ProcessPeople.Admin.success',null, null,function(){
+    console.log('incremented a stat')
+  });
+
+  //http request
   if(i%2==0) request.post(options1, callback);
   else request.post(options2, callback);
 }
